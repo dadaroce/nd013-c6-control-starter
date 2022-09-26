f2c3481f8111ee7dbd63d193715a27e01a39a60d
dadaroce <danielcediel97@gmail.com>
2022-09-26 01:24:39 -0500

    [FEAT]: change pid values

--- project/pid_controller/main.cpp
+++ project/pid_controller/main.cpp
@@ -228,8 +228,8 @@ int main ()
   PID pid_steer = PID();
   PID pid_throttle = PID();
 
-  pid_steer.Init(0.75, 0.23, 0.0, 1.2, -1.2);
-  pid_throttle.Init(0.5, 0.23, 0.0, 1.0, -1.0);
+  pid_steer.Init(0.5, 0.02, 0.3, 1.2, -1.2);
+  pid_throttle.Init(0.18, 0.01, 0.1, 1.0, -1.0);
 
 
   h.onMessage([&pid_steer, &pid_throttle, &new_delta_time, &timer, &prev_timer, &i, &prev_timer](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode)
